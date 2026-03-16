41e724c17135620747e8be9fa217c1b8f2dd8e19
Keegan Carlo Falcao <keegancarlo.falcao@studio.unibo.it>
2026-03-16 23:24:34 +0100

    creating

--- PythonServer/step-by-step/script.js
+++ PythonServer/step-by-step/script.js
@@ -1,12 +1,17 @@
 //Binds the load button to the getData function
 document.getElementById("loadBtn").addEventListener("click", getData);
 
+document.getElementById("clearBtn").addEventListener("click", function () {
+  document.getElementById("outPara").innerText = "";
+});
+
 //Fetches data from backend trough the API endpoint and updates the paragraph text with the response
 function getData() {
-  fetch("http://localhost:5000/api/quote")
+  fetch("http://localhost:5000/api/data")
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("outPara").innerText = data.quote;
+      document.getElementById("outPara").innerText =
+        data.message + " (" + data.status + ")";
     })
     .catch((error) => console.error("Error:", error));
 }
