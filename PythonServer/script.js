4c539bb12164f053d436715a2f9cf4f727dd4fa3
Keegan Carlo Falcao <keegancarlo.falcao@studio.unibo.it>
2026-03-16 22:46:16 +0100

    heavily

--- PythonServer/script.js
+++ PythonServer/script.js
@@ -1,9 +1,13 @@
 //Binds the load button to the getData function
 document.getElementById("loadBtn").addEventListener("click", getData);
 
+document.getElementById("clearBtn").addEventListener("click", function () {
+  document.getElementById("outPara").innerText = "";
+});
+
 //Fetches data from backend trough the API endpoint and updates the paragraph text with the response
 function getData() {
-  fetch("/api/data")
+  fetch("http://localhost:5000/api/data")
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("outPara").innerText =
